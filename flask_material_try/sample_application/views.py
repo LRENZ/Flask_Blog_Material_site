301eb154cdea50ab137932a829ad8607044df23f
LRENZ <lrenz1116@gmail.com>
2018-04-26 22:06:02 +0800

    add new index.html

--- flask_material_try/sample_application/views.py
+++ flask_material_try/sample_application/views.py
@@ -1,15 +1,25 @@
 from flask import Blueprint, render_template, redirect, url_for
 
 
-bp = Blueprint('test', __name__)
+bp = Blueprint('blog', __name__)
 
 
 
 @bp.route('/')
 def index():
-    return render_template('new_test.html')
+    return render_template('index.html')
 	
-@bp.route('/image')
+@bp.route('/post')
 def image():
     return render_template('new_test.html')
+
+
+@bp.route('/detail')
+def post():
+    return render_template('post.html')
+
+
+@bp.route('/readme')
+def readme():
+    return render_template('readme.html')
     
