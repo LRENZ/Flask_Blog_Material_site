aa1269def7918ae67fbe37d4e6e9f2f7ed1f0da2
LRENZ <lrenz1116@gmail.com>
2018-06-06 21:51:06 +0800

    change wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from werkzeug.contrib.fixers import ProxyFix
-from app import create_app
+from sample_application import create_app
 
 app = create_app()
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0')
